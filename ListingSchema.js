a9811570d15741f2b563e75b8ad601f12742f7f1
carolhob4 <carolhob4@ufl.edu>
2019-09-18 17:42:21 -0400

    Finished bootcamp 2

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,5 +1,7 @@
-/* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+/* Caroline Hobson
+
+ Import mongoose and define any variables needed to create the schema */
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema for the data in the listings.json file that will define how data is saved in your database
@@ -7,16 +9,32 @@ var mongoose = require('mongoose'),
      See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
   */
 var listingSchema = new Schema({
-  /* Your code for a schema here */ 
+  /* Your code for a schema here */
   //Check out - https://mongoosejs.com/docs/guide.html
 
+  //Code and name must be required in order to create the query
+  code: {type: String, required: true},
+  name: {type: String, required: true},
+  coordinates: {latitude: Number, longitude: Number},
+  address: String,
+  created_at: Date,
+  updated_at: Date
 });
 
-/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
+/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property
    See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  //saves the timestamp of the schema
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+
+  if(!this.created_at){
+    this.created_at = currentDate;
+  }
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
--- queries.js
+++ queries.js
@@ -1,36 +1,75 @@
-/* Add all the required libraries*/
+/* Caroline Hobson
 
+*/
+'use strict';
+/* Add all the required libraries*/
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 /* Connect to your database using mongoose - remember to keep your key secret*/
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.find({name: 'Library West', code: 'LBW'}, function(err,entry){
+     if(err) throw err;
+
+     console.log(entry);
+   });
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+   Listing.findOneAndRemove({code: 'CABL'}, function(err){
+     //thows if cant find entry
+     if (err) throw err;
+      console.log('Deleted CABL');
+   });
 };
+
+
 var updatePhelpsLab = function() {
   /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Lab address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+   Listing.findOneAndUpdate({name: 'Phelps Laboratory'}, {address:'1953 Museum Rd, Gainesville, FL 32603, United States', coordinates: {latitude: 29.644890, longitude: -82.348834 }}, function(err, entry){
+     if(err) throw err;
+   });
+
+   Listing.find({name: 'Phelps Laboratory'}, function(err,entry){
+     if(err) throw err;
+
+     console.log(entry);
+   });
+
 };
+
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err, entry){
+     if (err) throw err;
+     //returns all entries
+     console.log(entry);
+   });
+
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
